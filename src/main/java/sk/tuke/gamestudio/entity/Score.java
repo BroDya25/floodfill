7b7bf263f0fc9ce2f80b100574e05f36291707c6
Your Name <bohdan.kopcha@student.tuke.sk>
2026-03-05 20:29:59 +0300

    add file

---
+++ src/main/java/sk/tuke/gamestudio/entity/Score.java
@@ -0,0 +1,7 @@
+package sk.tuke.gamestudio.entity;
+
+public class Score {
+    private String game;
+    private String player;
+    private int points;
+}
---
+++ src/main/java/sk/tuke/gamestudio/service/ServiceScore.java
@@ -0,0 +1,11 @@
+package sk.tuke.gamestudio.service;
+
+import sk.tuke.gamestudio.entity.Score;
+import java.util.List;
+
+public class ServiceScore {
+
+    void addScore(Score score) {}
+
+    List<Score> getTopScores(String gameName) { return null; }
+}
