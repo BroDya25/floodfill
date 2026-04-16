b93ba33d1593c37ba5209af448e6db662d5cedf1
Your Name <bohdan.kopcha@student.tuke.sk>
2026-04-16 13:42:59 +0400

    update file

--- src/main/java/sk/tuke/gamestudio/SpringClient.java
+++ src/main/java/sk/tuke/gamestudio/SpringClient.java
@@ -1,9 +1,14 @@
 package sk.tuke.gamestudio;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.game.floodfill.consoleui.ConsoleUI;
 import sk.tuke.gamestudio.game.floodfill.core.ColorType;
 import sk.tuke.gamestudio.service.*;
@@ -11,10 +16,12 @@ import sk.tuke.gamestudio.service.*;
 import java.util.Scanner;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "sk.tuke.gamestudio.server.*"))
 public class SpringClient {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringClient.class, args);
+        new SpringApplicationBuilder(SpringClient.class).web(WebApplicationType.NONE).run(args);
     }
 
     @Bean
@@ -64,16 +71,21 @@ public class SpringClient {
 
     @Bean
     public ScoreService scoreService() {
-        return new ScoreServiceJPA();
+        return new ScoreServiceRestClient();
     }
 
     @Bean
     public CommentService commentService() {
-        return new CommentServiceJPA();
+        return new CommentServiceRestClient();
     }
 
     @Bean
     public RatingService ratingService() {
-        return new RatingServiceJPA();
+        return new RatingServiceRestClient();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
